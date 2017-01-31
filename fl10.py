63717efe1cfee222093ef7e27673f570f4cb4739
Steve Siegel <stevesiegel4848@gmail.com>
2017-01-30 17:53:19 -0800

    Add files via upload

---
+++ fl10.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 17:23:33 2017
+
+@author: Steve Siegel
+"
+
+# fl10.py
+# Demo:
+# python fl10.py
+import os
+from   flask import Flask
+application = Flask(__name__)
+                               
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    application.run(host='0.0.0.0', port=port)
+'bye'
